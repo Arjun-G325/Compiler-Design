75231a16f505cae517a07aa547d596818a3e1284
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-08 12:00:26 +0530

    testcase2.c

---
+++ Lexer/test/testcase2.c
@@ -0,0 +1,16 @@
+int a = 123;
+float b = 3.14;
+float sci = 1.2e10;
+float sci2 = 1e-3;
+
+a = a + b - 2 * 4 / 2;
+if (a == b || a%b==0 || a != b && a >= b && a <= b) {
+  a++;
+--b;
+
+}
+
+
+int x = a & b | a ^ b;
+
+
