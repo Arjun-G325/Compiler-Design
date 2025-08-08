bbf2d911f76ba1bf62cc7d3f6c448940de1867d6
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-08 11:58:19 +0530

    testcase1.c

--- Lexer/test/testcase1.c
+++ Lexer/test/testcase1.c
@@ -1,1 +1,33 @@
+int main() {
+    int a;
+    char b;
+    float c;
+    double d;
+    void func();
+    signed int x;
+    unsigned int y;
+    
+    if (a == b) {
+        return;
+    } else if (x > y) {
+        for (int i = 0; i < 10; ++i) {
+            continue;
+        }
+    } else {
+        while (x < y) {
+            break;
+        }
+    }
+
+    goto label;
+    label: struct S { int m; };
+    typedef int myint;
+    static int s;
+    const char* str;
+    auto z = 42;
+    default:
+    switch (z) {
+        case 1: break;
+    }
+}
 
