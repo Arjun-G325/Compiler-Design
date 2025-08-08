d3cb5aeda1dfdf51e3a79294f18cb6a82a11e2b6
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-08 12:01:35 +0530

    testcase3.c

---
+++ Lexer/test/testcase3.c
@@ -0,0 +1,12 @@
+char c = 'x';
+char d = '\n';
+char e = '\\';
+char f = '\'';
+
+string msg = "Hello, world!";
+string esc = "Line1\nLine2\tTabbed";
+string quote = "\"Quoted\" string";
+
+int arr[5];
+arr[0] = 1;
+
