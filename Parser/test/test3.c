d9f61d04829ec2dfb95f0ce5b70866b74f6dc4ca
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-30 11:41:17 +0530

    Create test3.c

---
+++ Parser/test/test3.c
@@ -0,0 +1,27 @@
+struct Point {
+    int x;
+    int y;
+};
+
+union Data {
+    int i;
+    float f;
+};
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE
+};
+
+int main() {
+    struct Point p;
+    p.x = 10;
+    p.y = 20;
+
+    union Data d;
+    d.f = 3.14;
+
+    enum Color c = GREEN;
+    return p.x;
+}
