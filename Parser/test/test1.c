d6163978793905639257274eb037c5fd008936b0
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-30 11:40:11 +0530

    Update test1.c

--- Parser/test/test1.c
+++ Parser/test/test1.c
@@ -1,1 +1,17 @@
+int a;
+float b = 3.5;
+char c = 'x';
+unsigned int d = 42;
+const int e = 100;
+
+int main() {
+    int x = 10, y = 20;
+    x = x + y * 2;
+    if (x > y) {
+        x = x - 1;
+    } else {
+        x = x + 1;
+    }
+    return x;
+}
 
