f4963ebef389a9de5406d03cf174537e3e8f48c0
Sakura1711 <aggitanjali2005@gmail.com>
2025-08-31 15:33:13 +0530

    Create test13.c

---
+++ Parser/test/test13.c
@@ -0,0 +1,14 @@
+struct Point {
+    int x, y;
+};
+
+int distance(struct Point *p1, struct Point *p2) {
+    int dx = p1->x - p2->x;
+    int dy = p1->y - p2->y;
+    return dx*dx + dy*dy;
+}
+
+int main() {
+    struct Point pts[2] = {{0, 0}, {3, 4}};
+    return distance(&pts[0], &pts[1]);
+}
